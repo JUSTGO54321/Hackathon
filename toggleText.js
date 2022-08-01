929d321379b0b801f4368e07be03613a4cfb32e8
Peter Gao <pgao2289@gmail.com>
2022-08-01 10:30:56 -0400

    button

---
+++ toggleText.js
@@ -0,0 +1,5 @@
+const toggleText = () => {
+    let text = document.getElementById("toggleText");
+
+    text.style.display === "none" ? text.style.display = "block": text.style.display = "none";
+}
